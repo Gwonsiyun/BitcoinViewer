2b229b3c0e7315e80eb6c4ce414038c9bf7a0c10
gwonsiyun <tldbs0813@gmail.com>
2022-06-15 16:49:07 +0900

    teestaaa

--- src/Coloring.js
+++ src/Coloring.js
@@ -1,13 +1,12 @@
 function Coloring({text}){
     let priceUSD = text;
     let priceWON = (Math.round(priceUSD*1292.49*1000)/1000).toString();
-    console.log(priceWON);
     let a = priceWON.substring(0, priceWON.indexOf("."));
     let b = priceWON.substring(priceWON.indexOf("."));
 
-    return `${a}'<span>'${b}'</span>'`;
-    // return "";
+    return (
+        <span>{a}<span style={{color:"red",}}>{b}</span></span>
+    );
 }
-// {(Math.round((coin.quotes.USD.price)*1292.49*1000)/1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 
 export default Coloring
